2292ae9a6fb87296b91dac8f0035f647c4caf888
sam2774 <yf/JSEvp'c@.5mgJx"lIs6|3P>
2017-11-09 19:08:59 -0600

    Added ability to check parity bits

--- src/Cipher/BruteForce/DESBruteForceThread.java
+++ src/Cipher/BruteForce/DESBruteForceThread.java
@@ -10,56 +10,96 @@ import Cipher.DES;
  * A thread that will run DES over a given key space.
  */
 public class DESBruteForceThread implements Runnable {
-    private BigInteger keySpaceBegin;
-    private BigInteger keySpaceEnd;
-    private int[] cipherText;
-    private int[] plainText;
-    private DES cipher;
-    private AtomicBoolean keyFound;
-    AtomicReference<String> correctKey;
+
+
+	private BigInteger keySpaceBegin;
+	private BigInteger keySpaceEnd;
+	private int[] cipherText;
+	private int[] plainText;
+	private int[] parityBits;
+	private DES cipher;
+	private AtomicBoolean keyFound;
+	AtomicReference<String> correctKey;
 	AtomicInteger threadsCompleted;
 
-    /**
-     * Constructs a DES force thread for a given key space
-     * @param keySpaceBegin the beginning of the key space, the first key to check
-     * @param keySpaceEnd the boundary of the key space, the key before this one is the last to be checked
-     * @param cipherText the cipher text trying to be cracked
-     * @param plainText the corresponding plain text
-     * @param keyFound thread safe boolean that will signal the other threads if key is found
-     * @param correctKey the variable to assign the correct key if it is found
-     * @param threadsCompleted variable thread will increment once thread has finished executing
-     */
-    public DESBruteForceThread(BigInteger keySpaceBegin, BigInteger keySpaceEnd, int[] cipherText, 
-    		int[] plainText, AtomicBoolean keyFound, AtomicReference<String> correctKey,
-    		AtomicInteger threadsCompleted) {
-        this.keySpaceBegin = keySpaceBegin;
-        this.keySpaceEnd = keySpaceEnd;
-        this.cipherText = cipherText;
-        this.plainText = plainText;
-        this.keyFound = keyFound;
-        this.correctKey = correctKey;
-        this.threadsCompleted = threadsCompleted;
-        this.cipher = new DES(16);
-    }
+	/**
+	 * Constructs a DES force thread for a given key space
+	 *
+	 * @param keySpaceBegin
+	 *            the beginning of the key space, the first key to check
+	 * @param keySpaceEnd
+	 *            the boundary of the key space, the key before this one is the last to be checked
+	 * @param cipherText
+	 *            the cipher text trying to be cracked
+	 * @param plainText
+	 *            the corresponding plain text
+	 * @param keyFound
+	 *            thread safe boolean that will signal the other threads if key is found
+	 * @param correctKey
+	 *            the variable to assign the correct key if it is found
+	 * @param threadsCompleted
+	 *            variable thread will increment once thread has finished executing
+	 */
+	public DESBruteForceThread(BigInteger keySpaceBegin, BigInteger keySpaceEnd, int[] cipherText, int[] plainText, int[] parityBits, AtomicBoolean keyFound, AtomicReference<String> correctKey, AtomicInteger threadsCompleted)
+	{
+		this.keySpaceBegin = keySpaceBegin;
+		this.keySpaceEnd = keySpaceEnd;
+		this.cipherText = cipherText;
+		this.plainText = plainText;
+		this.parityBits = parityBits;
+		this.keyFound = keyFound;
+		this.correctKey = correctKey;
+		this.threadsCompleted = threadsCompleted;
+		this.cipher = new DES(16);
+	}
 
-    @Override
-    public void run() {
-    	BigInteger currentKey = keySpaceBegin;
-    	String currentKeyString;
-    	int[] cipherTextOfCurrentKey = new int[8];
-    	
-        while(!keyFound.get() && !keySpaceEnd.equals(currentKey)) {
-        	currentKeyString = currentKey.toString(2);
-        	while (currentKeyString.length() < 64)
-                currentKeyString = "0" + currentKeyString;
-            cipher.setKey(currentKeyString);
-            cipherTextOfCurrentKey = cipher.encrypt(plainText);
-            if(Arrays.equals(cipherTextOfCurrentKey, cipherText)) {
-            	keyFound.set(true);
-            	correctKey.set(currentKeyString); 
-            }
-            currentKey = currentKey.add(BigInteger.ONE);
-        }
-        threadsCompleted.incrementAndGet();
-    }
+	public Boolean checkKey(String key)
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			int num = 0;
+			for (int j = 0; j < 7; j++)
+			{
+				if (key.charAt(i * 7 + j) == '1')
+				{
+					num++;
+				}
+			}
+			int parity = num % 2;
+			if (parity != parityBits[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public void run()
+	{
+		BigInteger currentKey = keySpaceBegin;
+		String currentKeyString;
+		int[] cipherTextOfCurrentKey = new int[8];
+
+		while (!keyFound.get() && !keySpaceEnd.equals(currentKey))
+		{
+			currentKeyString = currentKey.toString(2);
+			while (currentKeyString.length() < 56)
+			{
+				currentKeyString = "0" + currentKeyString;
+				if (checkKey(currentKeyString))
+				{
+					cipher.setKey(currentKeyString);
+					cipherTextOfCurrentKey = cipher.encrypt(plainText);
+					if (Arrays.equals(cipherTextOfCurrentKey, cipherText))
+					{
+						keyFound.set(true);
+						correctKey.set(currentKeyString);
+					}
+				}
+			}
+			currentKey = currentKey.add(BigInteger.ONE);
+		}
+		threadsCompleted.incrementAndGet();
+	}
 }
--- src/Cipher/sockettest/Client.java
+++ src/Cipher/sockettest/Client.java
@@ -1,140 +1,148 @@
 package Cipher.sockettest;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.math.BigInteger;
 import java.net.Socket;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-
 import Cipher.BruteForce.DESBruteForceNode;
-
 
 public class Client {
 
-   private BufferedReader in;
-   private PrintWriter out;
 
-   public Client() {}
+	private BufferedReader in;
+	private PrintWriter out;
 
-   /**
-    * Once the client is connected, retrieve keyspace and begin
-    * running the algorithm.
-    */
-   private void start(String plaintext, String ciphertext) {
-       byte[] plainTextBytes = plaintext.getBytes();
-       int[] plainTextInt = new int[8];
-       int[] cipherTextInt = new int[8];
-       for (int aByte = 0; aByte < 8; aByte++){
-           plainTextInt[aByte] = Byte.toUnsignedInt(plainTextBytes[aByte]);
-           cipherTextInt[aByte] = Integer.parseInt(ciphertext.substring(aByte * 2, (aByte * 2) + 2), 16);
-       }
-       DESBruteForceNode node = new DESBruteForceNode(cipherTextInt, plainTextInt, 5);
-       boolean keyFound = false;
-       BigInteger keySpaceBegin;
-       BigInteger keySpaceEnd;
-       while (!serverFoundKey()) {
-           String keyspace = getResponse("key");
-           System.out.println("Keyspace: " + keyspace);
+	public Client()
+	{
+	}
 
-           String[] beginAndEnd = keyspace.split(",");
-           keySpaceBegin = new BigInteger(beginAndEnd[0]);
-           keySpaceEnd = new BigInteger(beginAndEnd[1]);
-           node.setKeyspace(keySpaceBegin, keySpaceEnd);
+	/**
+	 * Runs the client application.
+	 */
+	public static void main(String[] args) throws Exception
+	{
+		Client client = new Client();
+		client.connectToServer();
+	}
 
-           keyFound = node.run();
-           if (keyFound) {
-               // Tell server that key was found
-               getResponse("found," + node.getFoundKey());
-               break;
-           }
+	/**
+	 * Prompt the end user for the server's IP address, connecting, setting up streams, and consuming the welcome messages from the server.
+	 */
+	public void connectToServer() throws IOException
+	{
 
-           // Mark keyspace as complete
-           getResponse("complete");
-       }
+		// Get the server address from a dialog box.
+		String serverAddress = "13.58.224.85";
 
-       // Stop everything because key is found
-       getResponse(".");
-       System.exit(0);
+		// Make connection and initialize streams
+		Socket socket = new Socket(serverAddress, 9898);
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		out = new PrintWriter(socket.getOutputStream(), true);
 
-   }
+		// Consume the initial welcoming messages from the server
+		for (int i = 0; i < 3; i++)
+		{
+			System.out.println(in.readLine());
+		}
 
-   /**
-    * Ask server if anyone has found the key yet
-    */
-   private boolean serverFoundKey() {
-       String found = getResponse("check");
-       if (found.equals("true")) {
-           System.out.println("Key: " + getResponse("getKey"));
-           return true;
-       } else {
-           System.out.println("Key has not been found yet. "
-                   + "Response: " + found);
-           return false;
-       }
-   }
+		String plaintext = in.readLine();
+		System.out.println("plaintext: " + plaintext);
+		String ciphertext = in.readLine();
+		System.out.println("ciphertext: " + ciphertext);
+		String parityBits = in.readLine();
+		System.out.println("parity bits: " + parityBits);
 
-   /**
-    * Send a message to the server and read the response
-    *
-    * Values for msg:
-    * 'check': will return true/false depending on if key has been found
-    * 'key': will return new keyspace for this client to use
-    * '.': will end connection with server
-    * 'found': will tell server that this client found key
-    * 'complete': will tell server that client completed assigned keyspace
-    */
-   private String getResponse(String msg) {
-       out.println(msg);
-       System.out.println("Sent to server: " + msg);
-       String response;
-       try {
-           response = in.readLine();
-           if (response == null) {
-               System.exit(0);
-           }
-       } catch(IOException ex) {
-           response = "Error: " + ex;
-       }
-       return response;
-   }
+		start(plaintext, ciphertext, parityBits);
+	}
 
-   /**
-    * Prompt the end user for the server's IP address, connecting,
-    * setting up streams, and consuming the welcome messages from the server.
-    */
-   public void connectToServer() throws IOException {
+	/**
+	 * Send a message to the server and read the response
+	 *
+	 * Values for msg: 'check': will return true/false depending on if key has been found 'key': will return new keyspace for this client to use '.': will end connection with server 'found': will tell server that this client found key 'complete':
+	 * will tell server that client completed assigned keyspace
+	 */
+	private String getResponse(String msg)
+	{
+		out.println(msg);
+		System.out.println("Sent to server: " + msg);
+		String response;
+		try
+		{
+			response = in.readLine();
+			if (response == null)
+			{
+				System.exit(0);
+			}
+		}
+		catch (IOException ex)
+		{
+			response = "Error: " + ex;
+		}
+		return response;
+	}
 
-       // Get the server address from a dialog box.
-       String serverAddress = "13.58.224.85";
+	/**
+	 * Ask server if anyone has found the key yet
+	 */
+	private boolean serverFoundKey()
+	{
+		String found = getResponse("check");
+		if (found.equals("true"))
+		{
+			System.out.println("Key: " + getResponse("getKey"));
+			return true;
+		}
+		else
+		{
+			System.out.println("Key has not been found yet. " + "Response: " + found);
+			return false;
+		}
+	}
 
-       // Make connection and initialize streams
-       Socket socket = new Socket(serverAddress, 9898);
-       in = new BufferedReader(
-               new InputStreamReader(socket.getInputStream()));
-       out = new PrintWriter(socket.getOutputStream(), true);
+	/**
+	 * Once the client is connected, retrieve keyspace and begin running the algorithm.
+	 */
+	private void start(String plaintext, String ciphertext, String parityBitsString)
+	{
+		byte[] plainTextBytes = plaintext.getBytes();
+		int[] plainTextInt = new int[8];
+		int[] cipherTextInt = new int[8];
+		int[] parityBits = new int[8];
+		for (int aByte = 0; aByte < 8; aByte++)
+		{
+			plainTextInt[aByte] = Byte.toUnsignedInt(plainTextBytes[aByte]);
+			cipherTextInt[aByte] = Integer.parseInt(ciphertext.substring(aByte * 2, (aByte * 2) + 2), 16);
+			parityBits[aByte] = Integer.parseInt(parityBitsString.split(",")[aByte]);
+		}
+		DESBruteForceNode node = new DESBruteForceNode(cipherTextInt, plainTextInt, parityBits, 5);
+		boolean keyFound = false;
+		BigInteger keySpaceBegin;
+		BigInteger keySpaceEnd;
+		while (!serverFoundKey())
+		{
+			String keyspace = getResponse("key");
+			System.out.println("Keyspace: " + keyspace);
 
-       // Consume the initial welcoming messages from the server
-       for (int i = 0; i < 3; i++) {
-           System.out.println(in.readLine());
-       }
+			String[] beginAndEnd = keyspace.split(",");
+			keySpaceBegin = new BigInteger(beginAndEnd[0]);
+			keySpaceEnd = new BigInteger(beginAndEnd[1]);
+			node.setKeyspace(keySpaceBegin, keySpaceEnd);
 
-       String plaintext = in.readLine();
-       System.out.println("plaintext: " + plaintext);
-       String ciphertext = in.readLine();
-       System.out.println("ciphertext: " + ciphertext);
+			keyFound = node.run();
+			if (keyFound)
+			{
+				// Tell server that key was found
+				getResponse("found," + node.getFoundKey());
+				break;
+			}
 
-       start(plaintext, ciphertext);
-   }
+			// Mark keyspace as complete
+			getResponse("complete");
+		}
 
-   /**
-    * Runs the client application.
-    */
-   public static void main(String[] args) throws Exception {
-       Client client = new Client();
-       client.connectToServer();
-   }
+		// Stop everything because key is found
+		getResponse(".");
+		System.exit(0);
+
+	}
 }
